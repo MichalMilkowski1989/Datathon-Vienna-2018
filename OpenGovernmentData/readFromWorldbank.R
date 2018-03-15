f7b8e28719700d07e70d46b8f9714142a5056d0a
sk6108 <Sarah.klein@thinkbiganalytics.com>
2018-03-15 08:16:40 +0100

    create long lists of all indicators in one file

--- OpenGovernmentData/readFromWorldbank.R
+++ OpenGovernmentData/readFromWorldbank.R
@@ -7,45 +7,50 @@ GenderStatsIndicators <- fromJSON('https://api.worldbank.org/v2/sources/14/indic
 GenderStats <- list()
 for(i in GenderStatsIndicators[[2]]$id){
   print(paste('I start now with... ',i))
-  GenderStats[[i]] <- fromJSON(paste0('http://api.worldbank.org/countries/all/indicators/',i,'/?format=json&date=1960:2017&per_page=20000'))  
-  write.csv(jsonlite::flatten(GenderStats[[i]][[2]]),file = paste0('GenderStats_',i,'.csv'))
+  zw <- fromJSON(paste0('http://api.worldbank.org/countries/all/indicators/',i,'/?format=json&date=1960:2017&per_page=20000'))  
+  zw <- jsonlite::flatten(zw[[2]])
+  GenderStats[[i]] <- zw
+  #write.csv(zw,file = paste0('GenderStats_',i,'.csv'))
 }
+GenderStatsLonglist <- bind_rows(GenderStats)
+write.csv(GenderStatsLonglist,file = 'GenderStats_LL.csv')
 
 SustainableDevelopmentGoalsIndicators <- fromJSON('https://api.worldbank.org/v2/sources/46/indicators?format=json') # annual til 2014
 SustainableDevelopmentGoals <- list()
 for(i in SustainableDevelopmentGoalsIndicators[[2]]$id){
   print(paste('I start now with... ',i))
-  SustainableDevelopmentGoals[[i]] <- fromJSON(paste0('http://api.worldbank.org/countries/all/indicators/',i,'/?format=json&date=1960:2017&per_page=20000'))  
-  write.csv(jsonlite::flatten(SustainableDevelopmentGoals[[i]][[2]]),file = paste0('SustainableDevelopmentGoals_',i,'.csv'))
+  zw <- fromJSON(paste0('http://api.worldbank.org/countries/all/indicators/',i,'/?format=json&date=1960:2017&per_page=20000'))  
+  zw <- jsonlite::flatten(zw[[2]])
+  SustainableDevelopmentGoals[[i]] <- zw
+  #write.csv(jsonlite::flatten(SustainableDevelopmentGoals[[i]][[2]]),file = paste0('SustainableDevelopmentGoals_',i,'.csv'))
 }
+SustainableDevelopmentGoalsLonglist <- bind_rows(SustainableDevelopmentGoals)
+write.csv(SustainableDevelopmentGoalsLonglist,file = 'SustainableDevelopmentGoals_LL.csv')
 
 WealthAccountingIndicators <- fromJSON('https://api.worldbank.org/v2/sources/59/indicators?format=json')   # every 5 years only
 WealthAccounting <- list()
 for(i in WealthAccountingIndicators[[2]]$id){
   print(paste('I start now with... ',i))
-  WealthAccounting[[i]] <- fromJSON(paste0('http://api.worldbank.org/countries/all/indicators/',i,'/?format=json&date=1960:2017&per_page=20000'))  
-  write.csv(jsonlite::flatten(WealthAccounting[[i]][[2]]),file = paste0('WealthAccounting_',i,'.csv'))
+  zw <- fromJSON(paste0('http://api.worldbank.org/countries/all/indicators/',i,'/?format=json&date=1960:2017&per_page=20000'))  
+  zw <- jsonlite::flatten(zw[[2]])
+  WealthAccounting[[i]]  <- zw
+  #write.csv(jsonlite::flatten(WealthAccounting[[i]][[2]]),file = paste0('WealthAccounting_',i,'.csv'))
 }
+WealthAccountingLonglist <- bind_rows(WealthAccounting)
+write.csv(WealthAccountingLonglist,file = 'WealthAccounting_LL.csv')
+
+
 
 GlobalEconomicMonitorIndicators <- fromJSON('http://api.worldbank.org/v2/indicators?format=json&per_page=20000')
 GlobalEconomicMonitor <- list()
-
 for(i in GlobalEconomicMonitorIndicators[[2]]$id){
   print(paste('I start now with... ',i))
-  GlobalEconomicMonitor[[i]] <- fromJSON(paste0('http://api.worldbank.org/countries/all/indicators/',i,'/?format=json&date=1960:2017&per_page=20000'))  
-  write.csv(jsonlite::flatten(GlobalEconomicMonitor[[i]][[2]]),file = paste0('GlobalEconomicMonitor_',i,'.csv'))
+  zw <- fromJSON(paste0('http://api.worldbank.org/countries/all/indicators/',i,'/?format=json&date=1960:2017&per_page=20000'))  
+  zw <- jsonlite::flatten(zw[[2]])
+  GlobalEconomicMonitor[[i]]  <- zw
+ # write.csv(jsonlite::flatten(GlobalEconomicMonitor[[i]][[2]]),file = paste0('GlobalEconomicMonitor_',i,'.csv'))
 }
+GlobalEconomicMonitorLonglist <- bind_rows(GlobalEconomicMonitor)
+write.csv(GlobalEconomicMonitorLonglist,file = 'GlobalEconomicMonitor_LL.csv')
 
 
-
-GenderStatsMerged <- bind_rows(lapply(1:length(GenderStats), function(i) jsonlite::flatten(GenderStats[[i]][[2]])))
-write.csv(GenderStatsMerged,file = 'GenderStats.csv',row.names = F)
-
-GlobalEconomicMonitorMerged <- bind_rows(lapply(1:length(GlobalEconomicMonitor), function(i) jsonlite::flatten(GlobalEconomicMonitor[[i]][[2]])))
-write.csv(GlobalEconomicMonitorMerged,file = 'GlobalEconomicMonitor.csv',row.names = F)
-
-WealthAccountingMerged <- bind_rows(lapply(1:length(WealthAccounting), function(i) jsonlite::flatten(WealthAccounting[[i]][[2]])))
-write.csv(WealthAccounting,file = 'WealthAccounting.csv',row.names = F)
-
-SustainableDevelopmentGoalsMerged <- bind_rows(lapply(1:length(SustainableDevelopmentGoals), function(i) jsonlite::flatten(SustainableDevelopmentGoals[[i]][[2]])))
-write.csv(SustainableDevelopmentGoals,file = 'SustainableDevelopmentGoals.csv',row.names = F)
